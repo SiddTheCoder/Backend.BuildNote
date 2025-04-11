fe53c12c1bb52ba592f386d835031568e062e4fe
SiddTheCoder <siddthecoder@gmail.com>
2025-04-11 17:22:03 +0545

    Remove commented dotenv configuration path from index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import connectDB from "./db/index.js";
 import app from "./app.js";
 import dotenv from 'dotenv'
 
-dotenv.config({
-    path: './.env',
-})
+// dotenv.config({
+//     path: './.env',
+// })
+
+dotenv.config();
 
 await connectDB()
   .then(() => {
