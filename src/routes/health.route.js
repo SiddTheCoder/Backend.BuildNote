a6ab2f703df50f6bedec0ca44554cbef298d7739
SiddTheCoder <siddthecoder@gmail.com>
2025-04-11 18:05:25 +0545

    Fix syntax error in route definition for /ping endpoint in health.route.js

--- src/routes/health.route.js
+++ src/routes/health.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
 
-router.route.get("/ping", (req, res) => {
+router.route("/ping").get((req, res) => {
   res.status(200).send("Pong!");
 });
 
